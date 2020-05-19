0075fa7b0b8140097281a923e5aeebe26eb6153c
Jason <jason.sinc95@gmail.com>
2020-05-19 08:12:59 +0000

    forms updates

--- application/forms.py
+++ application/forms.py
@@ -2,6 +2,37 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
+
+class ApplicationsForm(FlaskForm):
+    job_id = StringField('Job id',
+            validators = [
+                DataRequired(),
+                Length(min=1, max=60)
+            ]
+    )
+    first_name = StringField('first Name',
+            validators = [
+                DataRequired(),
+                Length(min=3, max=60)
+            ]
+    )
+                
+    last_name = StringField('Last name',
+            validators = [
+                DataRequired(),
+                Length(min=4, max=60)
+            ]
+    )  
+                
+    email = StringField('Email Address',
+            validators = [
+                DataRequired(),
+                Length(min=4, max=60)
+            ]
+    )
+    submit = SubmitField('submit application')
+
+    
 class JobPostForm(FlaskForm):
     title = StringField('Job title/role',
             validators = [
@@ -46,4 +77,48 @@ class JobPostForm(FlaskForm):
     )
 
     submit = SubmitField('Post Job')
+
+class UpdateJobPostForm(FlaskForm):
+    title = StringField('Job title/role',
+            validators = [
+                DataRequired(),
+                Length(min=4, max=60)
+            ]
+    )
+
+    positions = StringField('positions',
+            validators = [
+                DataRequired(),
+                Length(min=1, max=4)
+            ]
+    )
+
+    years_of_experience = StringField('how many years of experience',
+            validators = [
+                DataRequired(),
+                Length(min=1, max=5)
+            ]
+    )
+
+    salary = StringField('annual salary of position',
+            validators = [
+                DataRequired(),
+                Length(min=4, max=100)
+            ]	
+    )
+
+    address = StringField('address of location',
+            validators = [
+                DataRequired(),
+                Length(min=7, max=100)
+            ]
+    )
+
+    postcode = StringField('postcode of location',
+            validators = [
+                DataRequired(),
+                Length(min=6, max=10)
+            ]
+    )
+    submit = SubmitField('Update job post')
 
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,7 @@
 from application import app, db
 from flask import render_template, redirect, url_for, request
 from application.models import JobPosts
-from application.forms import JobPostForm
+from application.forms import JobPostForm, ApplicationsForm
 
 @app.route('/')
 @app.route('/home')
@@ -15,7 +15,20 @@ def about():
 
 @app.route('/JobApplication')
 def job_application():
-	return render_template('job_application.html', title='Job Applications')
+	form = ApplicationsForm()
+	if form.validate_on_submit():
+		postData = JobApplications( 
+			job_id=form.job_id.data,
+			first_name=(form.first_name.data),
+			last_name=(form.last_name.data),
+			email=(form.email.data),	
+		)
+		db.session.add(postData)
+		db.session.commit()
+		return redirect(url_for('home'))
+	else:
+		print(form.errors)
+	return render_template('job_application.html', title='Job Applications', form=form)
 
 @app.route('/JobPostForm', methods=['GET', 'POST'])
 def post():
@@ -23,9 +36,9 @@ def post():
 	if form.validate_on_submit():
 		postData = JobPosts( 
 			title=form.title.data,
-			positions=form.positions.data,
-			years_of_experience=form.years_of_experience.data,
-			salary=form.salary.data,
+			positions=int(form.positions.data),
+			years_of_experience=int(form.years_of_experience.data),
+			salary=int(form.salary.data),
 			address=form.address.data,
 			postcode=form.postcode.data		
 		)
