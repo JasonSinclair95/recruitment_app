729eb157f3e37e9d8ddda2beed629272a45b0a1c
Jason <jason.sinc95@gmail.com>
2020-05-22 13:57:29 +0000

    jenkins running app on Gunicon

--- script/installation.sh
+++ script/installation.sh
@@ -16,5 +16,5 @@ source /var/lib/jenkins/workspace/example_freestyle/venv/bin/activate
 
 pip3 install -r /var/lib/jenkins/workspace/example_freestyle/requirements.txt
 
-python3 /var/lib/jenkins/workspace/example_freestyle/app.py
+gunicorn --bind=0.0.0.0:5000 app:app
 
