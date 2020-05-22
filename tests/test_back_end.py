9eeaa17ee82a3874ecd11fa2b162b281c0738c0a
Jason <jason.sinc95@gmail.com>
2020-05-22 12:18:07 +0000

    pytest cov  at 84%

--- tests/test_back_end.py
+++ tests/test_back_end.py
@@ -13,64 +13,61 @@ class TestBase(TestCase):
 
         # pass in configurations for test database
         config_name = 'testing'
-        app.config.update(RECRUITMENT_VM_DB_URI=getenv('TEST_DB_URI'),
+        app.config.update(SQLALCHEMY_DATABASE_URI=getenv('TEST_DB_URI'),
                 SECRET_KEY=getenv('TEST_SECRET_KEY'),
-                WTF_CSRF_ENABLE=False,
+                WTF_CSRF_ENABLED=False,
                 DEBUG=True
                 )
         return app
+    def setUp(self):
+        """
+        Will be called before every test
+        """
+        # ensure there is no data in the test database when the test starts
+        db.session.commit()
+        db.drop_all()
+        db.create_all()
+
+        job1 = JobPosts(
+            job_role='Aws Specialist', 
+            positions='1', 
+            years_of_experience='3', 
+            salary='35000', 
+            address='7 London Road', 
+            postcode='se165p' 
+        )
+        job2 = JobPosts(
+            job_role='Devops Eningeer', 
+            positions='1', 
+            years_of_experience='4', 
+            salary='55000', 
+            address='7 London Road', 
+            postcode='se165p' 
+        )
+        db.session.add(job1)
+        db.session.add(job2)
+        db.session.commit()
 
     def tearDown(self):
-        """
-        Will be called after every test
-        """
         db.session.remove()
         db.drop_all()
     
 class TestViews(TestBase):
-
     def test_homepage_view(self):
-        """
-        Test that homepage is accessible 
-        """
         response = self.client.get(url_for('home'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_about_view(self):
+        response = self.client.get(url_for('about'))
         self.assertEqual(response.status_code, 200)
 
 class TestPosts(TestBase):
-
     def test_add_new_job_post(self):
-        """
-        Test that when I add a new post, I am redirected to the homepage with the new post visible
-        """
         with self.client:
             response = self.client.post(
-                '/jobs_post',
+                '/JobPostForm',
                 data=dict(
-                    title="Test Title",
-                    content="Test Content",
-                    job_role='Aws Specialist', 
-                    positions='1', 
-                    years_of_experience='3', 
-                    salary='35000', 
-                    address='7 London Road', 
-                    postcode='se165p'
-                ),
-            follow_redirects=True
-            )
-            self.assertIn(b'Test title', response.data)
-
-class TestAplication(TestBase):
-    def test_add_job_application(self):
-        """
-        Test that when I add a job applicaton, I am redirected to the homepage.
-        """
-        with self.client:
-            response = self.client.post(
-                '/jobs_post',
-                data=dict(
-                    title="Test Title",
-                    content="Test Content",
-                    job_role='Aws Specialist', 
+                    job_role='Test', 
                     positions='1', 
                     years_of_experience='3', 
                     salary='35000', 
@@ -79,4 +76,40 @@ class TestAplication(TestBase):
                 ),
                 follow_redirects=True
             )
-    
+            self.assertIn(b'Test', response.data)
+
+class TestAplication(TestBase):
+    def test_add_job_application(self):
+        with self.client:
+            response = self.client.post(
+                '/job_application/1/',
+                data=dict(
+                    first_name="test name",
+                    last_name="Test last name",
+                    email='test email', 
+                ),
+                follow_redirects=True
+            )
+            count = JobApplications.query.count()
+            self.assertEquals(count, 1)
+
+class TestRoutes(TestBase):
+    def test_update(self):
+        with self.client:
+            response = self.client.post(
+                '/UpdateJobPost/1/?',
+                data=dict(
+                    salary="50000"
+                ),
+                follow_redirects=True
+            )
+            self.assertIn(b'50000', response.data)
+ 
+    def test_delete(self):
+        with self.client:
+            response = self.client.post(
+                '/Delete/1/JobPost/',
+                follow_redirects=True
+            )
+            count = JobPosts.query.count()
+            self.assertEquals(count, 1)
